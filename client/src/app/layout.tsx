f8ca51c14c0bdcefe09889261a684e9ef6d72457
prakshh <prakashdas18@gmail.com>
2024-12-13 13:03:06 +0530

    setting redux

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
+import Providers from "./providers";
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
@@ -20,10 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${dmSans.className}`}
-      >
-        {children}
+      <body className={`${dmSans.className}`}>
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
