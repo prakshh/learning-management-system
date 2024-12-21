5d94cc0c6ff10a505f4203e583094012d362d92e
prakshh <prakashdas18@gmail.com>
2024-12-21 19:51:34 +0530

    settings page - toast message and error handling

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
@@ -28,6 +29,7 @@ export default function RootLayout({
             <div className="root-layout">
               {children}
             </div>
+            <Toaster richColors closeButton />
           </Providers>
         </body>
       </html>
